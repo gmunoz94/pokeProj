e767751872ef8df11e1db0f11dfba062905660ae
unknown <marcodotr@gmail.coom>
2021-06-03 21:16:09 -0500

    api for pokemon name integration

---
+++ js/script.js
@@ -0,0 +1,34 @@
+
+
+function inputval() {
+    var searchpoke = document.querySelector("#pokesearch").value;
+    pokename(searchpoke);
+    console.log(searchpoke);
+    document.querySelector("#pokesearch").value = "";
+}
+document.querySelector("#searchbtn").addEventListener("click",inputval)
+
+
+function pokename(searchpoke) {
+ var endpointURL = `https://pokeapi.co/api/v2/pokemon/${searchpoke}`
+
+$.ajax({
+    url: endpointURL,
+    method:"GET"
+}).then(function(apiResponce){
+    console.log(apiResponce)
+    var pokename = $('<h5>').text(apiResponce.name)
+    console.log(pokename)
+    
+    for (let i = 0; i < apiResponce.moves.length; i++) {
+        console.log("hello")
+        var moves = $('<h3>').text(apiResponce.moves[i].move.name)
+        $('#nameofpoke').append(moves)
+        console.log(moves)
+    }
+
+    //var moves = $('<h3>').text(apiResponce.moves.move.name)
+    //console.log(moves)
+    $('#nameofpoke').append(pokename)
+})
+}
